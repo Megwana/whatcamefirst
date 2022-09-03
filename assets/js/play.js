3f1e7fa7800e79c83915d4bc1204a2182a879db0
Megan <megantech@hotmail.com>
2022-09-03 14:59:06 +0000

    add initial const ids

---
+++ assets/js/play.js
@@ -0,0 +1,5 @@
+const startButton = document.getElementById('start-btn')
+const previousButton = document.getElementById('prev-btn')
+const nextButton = document.getElementById('next-btn')
+const submitButton = document.getElementById('submit-btn')
+const 
