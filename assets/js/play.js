cb1e7d07edc12148dbd0ea26156b50fb50f8d311
Megan <megantech@hotmail.com>
2022-09-03 15:12:17 +0000

    add question structure on play.html

--- assets/js/play.js
+++ assets/js/play.js
@@ -3,4 +3,9 @@ const previousButton = document.getElementById('prev-btn')
 const nextButton = document.getElementById('next-btn')
 const submitButton = document.getElementById('submit-btn')
 const quizContainerElement = document.getElementById('quiz-container')
-const 
+
+const questions = [
+    {
+        
+    }
+]
