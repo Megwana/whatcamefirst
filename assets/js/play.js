f3941c03e9e9c725e64b309f74555eac078bf6db
Megan <megantech@hotmail.com>
2022-09-07 11:22:12 +0000

    add answer query selector all

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,5 +1,5 @@
 const question = document.querySelector('#question');
-const choices = document.querySelector('');
+const answers = document.querySelectorAll('.answ-text');
 const points = document.querySelector('#points');
 const questionNumber = document.querySelector('#quest-num');
 const nextButton = document.getElementById('next-btn');
