d2fff1b3aa51912f816176b86eda07be6dd5be05
Megan <megantech@hotmail.com>
2022-09-07 14:42:27 +0000

    changed layout of js quiz to compare

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,106 +1,40 @@
-const question = document.querySelector('#question');
-const answers = Array.from(document.querySelectorAll('.answ-text'));
 const pointsDigits = document.querySelector('#points');
 const questionNumber = document.querySelector('#quest-num');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
 const nextButton = document.getElementById('next-btn');
+const answerElement = document.getElementById('answ-text');
 
-let currentQuestion = {}
-let acceptingAnswers = true
-let points = 0
-let questionCounter = 0
-let availableQuestions = []
+let shuffledQuestions, currentQuestionIndex
 
-const questions = [
-    {
-        question: 'What was founded first?',
-        answers: [
-        {1: 'Oxford University', correct: true},
-        {2: 'Aztec Empire', correct: false},
-        {3: 'Cambridge University', correct: false},
-        {4: 'Inca Empire', correct: false} ],
-
-        question: 'What was founded first?',
-        answers: [
-        {1: 'Oxford University', correct: true},
-        {2: 'Aztec Empire', correct: false},
-        {3: 'Cambridge University', correct: false},
-        {4: 'Inca Empire', correct: false} ],
-
-        question: 'What was founded first?',
-        answers: [
-        {1: 'Oxford University', correct: true},
-        {2: 'Aztec Empire', correct: false},
-        {3: 'Cambridge University', correct: false},
-        {4: 'Inca Empire', correct: false} ],
-
-        question: 'What was founded first?',
-        answers: [
-        {1: 'Oxford University', correct: true},
-        {2: 'Aztec Empire', correct: false},
-        {3: 'Cambridge University', correct: false},
-        {4: 'Inca Empire', correct: false} ],
-
-        question: 'What was founded first?',
-        answers: [
-        {1: 'Oxford University', correct: true},
-        {2: 'Aztec Empire', correct: false},
-        {3: 'Cambridge University', correct: false},
-        {4: 'Inca Empire', correct: false} ],
-
-        question: 'What was founded first?',
-        answers: [
-        {1: 'Oxford University', correct: true},
-        {2: 'Aztec Empire', correct: false},
-        {3: 'Cambridge University', correct: false},
-        {4: 'Inca Empire', correct: false} ],
-
-        question: 'What was founded first?',
-        answers: [
-        {1: 'Oxford University', correct: true},
-        {2: 'Aztec Empire', correct: false},
-        {3: 'Cambridge University', correct: false},
-        {4: 'Inca Empire', correct: false} ],
-
-        question: 'What was founded first?',
-        answers: [
-        {1: 'Oxford University', correct: true},
-        {2: 'Aztec Empire', correct: false},
-        {3: 'Cambridge University', correct: false},
-        {4: 'Inca Empire', correct: false} ],
-
-        question: 'What was founded first?',
-        answers: [
-        {1: 'Oxford University', correct: true},
-        {2: 'Aztec Empire', correct: false},
-        {3: 'Cambridge University', correct: false},
-        {4: 'Inca Empire', correct: false} ],
-
-        question: 'What was founded first?',
-        answers: [
-        {1: 'Oxford University', correct: true},
-        {2: 'Aztec Empire', correct: false},
-        {3: 'Cambridge University', correct: false},
-        {4: 'Inca Empire', correct: false} ],
-    }
-]
-
-const SCORE_POINTS = 20
-const MAX_QUESTIONS = 10
-
-runGame = () => {
+function runGame () {
     questionCounter = 0
     points = 0
     availableQuestions = [...questions]
-    NewQuestion()
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    addNewQuestion()
 }
 
-NewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-    localStorage.setItem('mostRecentPoints', points)
-
-    return window.location.assign('/end.html')
+function addNewQuestion () {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+function showQuestion(question) {
+    questionsElement.innerText = question.question
+    question.answers.forEach(answer => {
+        answer.innerText = answer.text
+        answer.classList.add('answ-text')
+        if (answer.correct) {
+            answer.dataset.correct = answer.correct
+        }
+        answer.addEventListener('click', selectAnswer)
+        answerElement.appendChild(answer)
+    })
+}
+
+/*
 questionCounter++
 questionNumber.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
 questionNumber.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
@@ -143,6 +77,79 @@ answers.forEach(answer => {
     })
 })
 
-runGame()
+*/
 
+let questions = [
+    {
+        question: 'What was founded first?',
+        answers: [
+        { text: 'Oxford University', correct: true},
+        { text: 'Aztec Empire', correct: false},
+        { text: 'Cambridge University', correct: false},
+        { text: 'Inca Empire', correct: false} ],
+
+        question: 'What was seen first?',
+        answers: [
+        { text: '1', correct: true},
+        { text: '2', correct: false},
+        { text: '3', correct: false},
+        { text: '4', correct: false} ],
+
+        question: 'What was founded first?',
+        answers: [
+        { text: '5', correct: true},
+        { text: '6', correct: false},
+        { text:'7', correct: false},
+        { text: '8', correct: false} ],
+
+        question: 'What was founded first?',
+        answers: [
+        {1: '9', correct: true},
+        {2: '10', correct: false},
+        {3: '11', correct: false},
+        {4: '12', correct: false} ],
+
+        question: 'What was founded first?',
+        answers: [
+        {1: '13', correct: true},
+        {2: '14', correct: false},
+        {3: '15', correct: false},
+        {4: '16', correct: false} ],
+
+        question: 'What was founded first?',
+        answers: [
+        {1: '17', correct: true},
+        {2: '22', correct: false},
+        {3: '86', correct: false},
+        {4: '69', correct: false} ],
+
+        question: 'What was hatched first?',
+        answers: [
+        {1: '45', correct: true},
+        {2: '74', correct: false},
+        {3: '92', correct: false},
+        {4: '36', correct: false} ],
+
+        question: 'What was created first?',
+        answers: [
+        {1: '564', correct: true},
+        {2: '23', correct: false},
+        {3: '479', correct: false},
+        {4: '28', correct: false} ],
+
+        question: 'What was founded first?',
+        answers: [
+        {1: 'OU', correct: true},
+        {2: 'AE', correct: false},
+        {3: 'CU', correct: false},
+        {4: 'IE', correct: false} ],
+
+        question: 'What was founded first?',
+        answers: [
+        {1: '98', correct: true},
+        {2: '384', correct: false},
+        {3: '395', correct: false},
+        {4: '937', correct: false} ],
+    }
+]
 
