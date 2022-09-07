26bc143e099f75440155af0c951e6dfffbbfa6c9
Megan <megantech@hotmail.com>
2022-09-07 09:36:08 +0000

    add answer c and d options

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,7 +1,4 @@
-const startButton = document.getElementById('start-btn')
-const previousButton = document.getElementById('prev-btn')
 const nextButton = document.getElementById('next-btn')
-const submitButton = document.getElementById('submit-btn')
 const quizContainerElement = document.getElementById('quiz-container')
 
 let shuffledQuestions, currentQuestionIndex
