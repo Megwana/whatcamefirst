acca87f4465e4721cc67168596b80783204ed76a
Megan <megantech@hotmail.com>
2022-09-10 13:10:50 +0000

    add js for modal-box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
-var modalButton = documnet.querySelector('.modal-btn');
+var modalButton = document.querySelector('#modal-btn');
 var modalBox = document.querySelector('.modal-box')
+
+modalButton.addEventListener('click',function(){
+    modalBox.classList.add('modal-active')
+});
