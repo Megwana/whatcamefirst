8e717a4f134a29713c78d746e7fc710b16fcd580
Megan <megantech@hotmail.com>
2022-09-10 13:18:16 +0000

    add js modal comment and color for button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+// Modal Box for Instructions on how to play the Quiz Game.
 var modalButton = document.querySelector('#modal-btn');
 var modalBox = document.querySelector('.modal-box')
 
 modalButton.addEventListener('click',function(){
     modalBox.classList.add('modal-active')
 });
+4
