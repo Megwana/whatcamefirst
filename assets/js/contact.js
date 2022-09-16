f76ed782c3d7387b21cbc4db130e82a98d8b7ce9
Megan <megantech@hotmail.com>
2022-09-16 23:52:39 +0000

    add securetoken smtpJS

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,18 +1,16 @@
 // Below function is from SmtpJS.com and credited in the ReadMe file
 function sendEmail() {
-    Email.send({
-    Host : "smtp.outlook.com",
-    Username : "megantech@hotmail.com",
-    Password : "WhatCameFirst",
+  Email.send({
+    SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
     To : 'megantech@hotmail.com',
     From : document.getElementById("email").value,
-    Subject : "WCF Quiz Enquiry",
+    Subject : "This is the subject",
     Body : "Name:" + document.getElementById("name").value
     + "<br> Email:" + document.getElementById("email").value
     + "<br> Phone Number:" + document.getElementById("phone").value
     + "<br> Message:" + document.getElementById("message").value
-
-}).then(
+  }).then(
   message => alert("Message Sent Successfully")
+  );
 );
 }
